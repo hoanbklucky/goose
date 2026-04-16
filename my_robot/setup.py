4b5b5f755201b3d7a100042bb45f6d935a784392
hoanbklucky <medicalaicompany@gmail.com>
2026-04-16 10:42:07 -0400

    ros2 implementation

---
+++ my_robot/setup.py
@@ -0,0 +1,8 @@
+entry_points={
+    'console_scripts': [
+        'camera_node = my_robot.camera_node:main',
+        'lane_node = my_robot.lane_node:main',
+        'control_node = my_robot.control_node:main',
+        'motor_node = my_robot.motor_node:main',
+    ],
+},
