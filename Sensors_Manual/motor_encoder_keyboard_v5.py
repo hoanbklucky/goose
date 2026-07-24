9cd53d2ea0d2555a333c52be6cffbae8dc666214
Silversan1235 <crazydj1235@gmail.com>
2026-07-24 15:18:08 -0400

    Update motor_encoder_keyboard_v5.py

--- Sensors_Manual/motor_encoder_keyboard_v5.py
+++ Sensors_Manual/motor_encoder_keyboard_v5.py
@@ -1,45 +1,38 @@
 #!/usr/bin/env python3
 
-import time
 import math
-import sys
 import threading
-import select
+import time
 import tty
 import termios
+import select
+import sys
 
 import gpiod
 
 import board
 import busio
-
 from adafruit_pca9685 import PCA9685
 
 
+# ==========================================================
+# PCA9685 CONFIGURATION
+# ==========================================================
+
+PWM_FREQUENCY = 1000
+
+LEFT_PWM_CHANNEL = 0
+RIGHT_PWM_CHANNEL = 1
+
+MAX_PWM = 4095
+DRIVE_SPEED = 2500
+
 
 # ==========================================================
-# ROBOT CONFIGURATION
+# MOTOR GPIO
 # ==========================================================
 
-
-# ----------------------------
-# GPIO CHIP
-# ----------------------------
-
-GPIO_CHIP = "/dev/gpiochip1"
-
-
-
-# ----------------------------
-# L298N MOTOR DRIVER GPIO
-# ----------------------------
-#
-# These are GPIO offsets
-# NOT physical header pin numbers
-#
-# Verify with:
-# gpioinfo gpiochip1
-#
+MOTOR_CHIP = "/dev/gpiochip1"
 
 IN1 = 7
 IN2 = 6
@@ -47,160 +40,125 @@ IN3 = 5
 IN4 = 4
 
 
+# ==========================================================
+# ENCODER GPIO
+# ==========================================================
 
-# ----------------------------
-# PCA9685
-# ----------------------------
+# Left encoder
+LEFT_A_CHIP = "/dev/gpiochip4"
+LEFT_A_LINE = 11      # Physical Pin 11
 
-PWM_FREQUENCY = 1000
+LEFT_B_CHIP = "/dev/gpiochip4"
+LEFT_B_LINE = 10      # Physical Pin 13
 
+# Right encoder
+RIGHT_A_CHIP = "/dev/gpiochip4"
+RIGHT_A_LINE = 12     # Physical Pin 15
 
-LEFT_PWM_CHANNEL = 0
-RIGHT_PWM_CHANNEL = 1
-
-
-MAX_PWM = 4095
-
-DRIVE_SPEED = 2500
-
+RIGHT_B_CHIP = "/dev/gpiochip1"
+RIGHT_B_LINE = 5      # Physical Pin 16
 
 
 # ==========================================================
-# PCA9685 INITIALIZATION
+# ENCODER CALIBRATION
 # ==========================================================
 
+COUNTS_PER_REV = 1092
+
+WHEEL_DIAMETER_IN = 2.5
+
+WHEEL_CIRCUMFERENCE = math.pi * WHEEL_DIAMETER_IN
+
+INCHES_PER_COUNT = (
+    WHEEL_CIRCUMFERENCE /
+    COUNTS_PER_REV
+)
+
+
+# ==========================================================
+# INITIALIZE PCA9685
+# ==========================================================
 
 i2c = busio.I2C(
     board.SCL,
     board.SDA
 )
 
-
 pca = PCA9685(i2c)
-
 
 pca.frequency = PWM_FREQUENCY
 
 
-
 def set_pwm(channel, value):
 
-    if value < 0:
-        value = 0
-
-    if value > MAX_PWM:
-        value = MAX_PWM
-
+    value = max(0, min(MAX_PWM, value))
 
     pca.channels[channel].duty_cycle = value
 
 
-
 # ==========================================================
-# LIBGPIOD 2.x MOTOR SETUP
+# MOTOR GPIO REQUEST
 # ==========================================================
-
 
 motor_request = gpiod.request_lines(
 
-    GPIO_CHIP,
+    MOTOR_CHIP,
 
-    consumer="RC_Car_Motor",
+    consumer="motor",
 
     config={
 
-        IN1:
-        gpiod.LineSettings(
-            direction=
-            gpiod.line.Direction.OUTPUT,
-
-            output_value=
-            gpiod.line.Value.INACTIVE
+        IN1: gpiod.LineSettings(
+            direction=gpiod.line.Direction.OUTPUT,
+            output_value=gpiod.line.Value.INACTIVE
         ),
 
-
-        IN2:
-        gpiod.LineSettings(
-            direction=
-            gpiod.line.Direction.OUTPUT,
-
-            output_value=
-            gpiod.line.Value.INACTIVE
+        IN2: gpiod.LineSettings(
+            direction=gpiod.line.Direction.OUTPUT,
+            output_value=gpiod.line.Value.INACTIVE
         ),
 
-
-        IN3:
-        gpiod.LineSettings(
-            direction=
-            gpiod.line.Direction.OUTPUT,
-
-            output_value=
-            gpiod.line.Value.INACTIVE
+        IN3: gpiod.LineSettings(
+            direction=gpiod.line.Direction.OUTPUT,
+            output_value=gpiod.line.Value.INACTIVE
         ),
 
-
-        IN4:
-        gpiod.LineSettings(
-            direction=
-            gpiod.line.Direction.OUTPUT,
-
-            output_value=
-            gpiod.line.Value.INACTIVE
+        IN4: gpiod.LineSettings(
+            direction=gpiod.line.Direction.OUTPUT,
+            output_value=gpiod.line.Value.INACTIVE
         )
-
     }
-)
 
+)
 
 
 def gpio_write(pin, state):
 
-    if state:
+    motor_request.set_value(
 
-        motor_request.set_value(
+        pin,
 
-            pin,
+        gpiod.line.Value.ACTIVE
+        if state
+        else
+        gpiod.line.Value.INACTIVE
 
-            gpiod.line.Value.ACTIVE
-
-        )
-
-    else:
-
-        motor_request.set_value(
-
-            pin,
-
-            gpiod.line.Value.INACTIVE
-
-        )
-
+    )
 
 
 # ==========================================================
-# BASIC MOTOR FUNCTIONS
+# MOTOR FUNCTIONS
 # ==========================================================
-
 
 def stop_motor():
 
     gpio_write(IN1, False)
     gpio_write(IN2, False)
-
     gpio_write(IN3, False)
     gpio_write(IN4, False)
 
-
-    set_pwm(
-        LEFT_PWM_CHANNEL,
-        0
-    )
-
-    set_pwm(
-        RIGHT_PWM_CHANNEL,
-        0
-    )
-
+    set_pwm(LEFT_PWM_CHANNEL, 0)
+    set_pwm(RIGHT_PWM_CHANNEL, 0)
 
 
 def forward(speed=DRIVE_SPEED):
@@ -211,17 +169,8 @@ def forward(speed=DRIVE_SPEED):
     gpio_write(IN3, True)
     gpio_write(IN4, False)
 
-
-    set_pwm(
-        LEFT_PWM_CHANNEL,
-        speed
-    )
-
-    set_pwm(
-        RIGHT_PWM_CHANNEL,
-        speed
-    )
-
+    set_pwm(LEFT_PWM_CHANNEL, speed)
+    set_pwm(RIGHT_PWM_CHANNEL, speed)
 
 
 def backward(speed=DRIVE_SPEED):
@@ -232,17 +181,8 @@ def backward(speed=DRIVE_SPEED):
     gpio_write(IN3, False)
     gpio_write(IN4, True)
 
-
-    set_pwm(
-        LEFT_PWM_CHANNEL,
-        speed
-    )
-
-    set_pwm(
-        RIGHT_PWM_CHANNEL,
-        speed
-    )
-
+    set_pwm(LEFT_PWM_CHANNEL, speed)
+    set_pwm(RIGHT_PWM_CHANNEL, speed)
 
 
 def turn_left(speed=DRIVE_SPEED):
@@ -253,17 +193,8 @@ def turn_left(speed=DRIVE_SPEED):
     gpio_write(IN3, True)
     gpio_write(IN4, False)
 
-
-    set_pwm(
-        LEFT_PWM_CHANNEL,
-        speed
-    )
-
-    set_pwm(
-        RIGHT_PWM_CHANNEL,
-        speed
-    )
-
+    set_pwm(LEFT_PWM_CHANNEL, speed)
+    set_pwm(RIGHT_PWM_CHANNEL, speed)
 
 
 def turn_right(speed=DRIVE_SPEED):
@@ -274,293 +205,347 @@ def turn_right(speed=DRIVE_SPEED):
     gpio_write(IN3, False)
     gpio_write(IN4, True)
 
-
-    set_pwm(
-        LEFT_PWM_CHANNEL,
-        speed
-    )
-
-    set_pwm(
-        RIGHT_PWM_CHANNEL,
-        speed
-    )
-
-
-
-# ==========================================================
-# PART 2
-# ENCODER SYSTEM (LIBGPIOD 2.x)
-# ==========================================================
-
-
-# ==========================================================
-# ENCODER CONFIGURATION
-# ==========================================================
-
-
-LEFT_ENCODER_A = 0
-LEFT_ENCODER_B = 13
-
-
-RIGHT_ENCODER_A = 2
-RIGHT_ENCODER_B = 3
-
-
-
-# Encoder calibration
-# From your previous test
-
-COUNTS_PER_REV = 1092
-
-
-WHEEL_DIAMETER_IN = 2.5
-
-
-WHEEL_CIRCUMFERENCE = (
-    math.pi *
-    WHEEL_DIAMETER_IN
-)
-
-
-INCHES_PER_COUNT = (
-    WHEEL_CIRCUMFERENCE /
-    COUNTS_PER_REV
-)
-
+    set_pwm(LEFT_PWM_CHANNEL, speed)
+    set_pwm(RIGHT_PWM_CHANNEL, speed)
 
 
 # ==========================================================
 # ENCODER CLASS
 # ==========================================================
 
-
 class Encoder:
-
 
     def __init__(
         self,
-        pin_a,
-        pin_b
+        chip_a,
+        line_a,
+        chip_b,
+        line_b
     ):
-
-
-        self.pin_a = pin_a
-        self.pin_b = pin_b
-
 
         self.count = 0
 
+        self.line_a = line_a
+        self.line_b = line_b
 
-        self.last_a = 0
+        self.request_a = gpiod.request_lines(
 
+            chip_a,
 
-
-        # Request GPIO inputs
-
-        self.request = gpiod.request_lines(
-
-            GPIO_CHIP,
-
-            consumer="RC_Car_Encoder",
+            consumer="encoder_a",
 
             config={
-
-
-                pin_a:
-
-                gpiod.LineSettings(
-
-                    direction=
-                    gpiod.line.Direction.INPUT
-
-                ),
-
-
-
-                pin_b:
-
-                gpiod.LineSettings(
-
-                    direction=
-                    gpiod.line.Direction.INPUT
-
+                line_a: gpiod.LineSettings(
+                    direction=gpiod.line.Direction.INPUT
                 )
-
             }
 
         )
 
+        self.request_b = gpiod.request_lines(
 
+            chip_b,
 
-        # Initial state
+            consumer="encoder_b",
 
-        self.last_a = self.request.get_value(
-            self.pin_a
+            config={
+                line_b: gpiod.LineSettings(
+                    direction=gpiod.line.Direction.INPUT
+                )
+            }
+
         )
 
-
-
+        self.last_a = self.request_a.get_value(
+            self.line_a
+        )
 
     def update(self):
 
-
-        a = self.request.get_value(
-            self.pin_a
+        a = self.request_a.get_value(
+            self.line_a
         )
 
-
-        b = self.request.get_value(
-            self.pin_b
+        b = self.request_b.get_value(
+            self.line_b
         )
-
-
-
-        # Quadrature decoding
 
         if a != self.last_a:
 
-
-            if b != a:
-
+            if a != b:
                 self.count += 1
-
-
             else:
-
                 self.count -= 1
 
-
-
         self.last_a = a
-
-
 
     def reset(self):
 
         self.count = 0
 
 
-
-
 # ==========================================================
 # CREATE ENCODERS
 # ==========================================================
 
-
 left_encoder = Encoder(
 
-    LEFT_ENCODER_A,
+    LEFT_A_CHIP,
+    LEFT_A_LINE,
 
-    LEFT_ENCODER_B
+    LEFT_B_CHIP,
+    LEFT_B_LINE
 
 )
-
-
 
 right_encoder = Encoder(
 
-    RIGHT_ENCODER_A,
+    RIGHT_A_CHIP,
+    RIGHT_A_LINE,
 
-    RIGHT_ENCODER_B
+    RIGHT_B_CHIP,
+    RIGHT_B_LINE
 
 )
 
-
-
-
 # ==========================================================
-# ENCODER UPDATE THREAD
+# ENCODER THREAD
 # ==========================================================
-
 
 encoder_running = True
 
 
-
 def encoder_loop():
-
 
     global encoder_running
 
-
-
     while encoder_running:
 
-
         left_encoder.update()
-
-
         right_encoder.update()
-
 
         time.sleep(0.001)
 
 
-
-
 encoder_thread = threading.Thread(
-
     target=encoder_loop,
-
     daemon=True
-
 )
-
 
 
 # ==========================================================
 # DISTANCE FUNCTIONS
 # ==========================================================
 
-
 def left_distance():
 
     return (
-
         left_encoder.count *
-
         INCHES_PER_COUNT
-
     )
-
-
 
 
 def right_distance():
 
     return (
-
         right_encoder.count *
-
         INCHES_PER_COUNT
-
     )
-
-
 
 
 def average_distance():
 
     return (
-
-        left_distance()
-
-        +
-
+        left_distance() +
         right_distance()
-
     ) / 2
-
-
 
 
 def reset_encoders():
 
-
     left_encoder.reset()
-
     right_encoder.reset()
 
 
+# ==========================================================
+# KEYBOARD SETUP
+# ==========================================================
+
+old_terminal = termios.tcgetattr(
+    sys.stdin
+)
 
 
+def keyboard_setup():
+
+    tty.setcbreak(
+        sys.stdin.fileno()
+    )
+
+
+def keyboard_restore():
+
+    termios.tcsetattr(
+        sys.stdin,
+        termios.TCSADRAIN,
+        old_terminal
+    )
+
+
+def key_pressed():
+
+    ready, _, _ = select.select(
+        [sys.stdin],
+        [],
+        [],
+        0
+    )
+
+    if ready:
+
+        return sys.stdin.read(1)
+
+    return None
+
+
+# ==========================================================
+# STATUS DISPLAY
+# ==========================================================
+
+def print_status():
+
+    print(
+        "\033[2J\033[H",
+        end=""
+    )
+
+    print("==============================")
+    print(" GOOSE RC CAR ")
+    print("==============================")
+    print()
+
+    print(
+        f"Left Encoder : {left_encoder.count}"
+    )
+
+    print(
+        f"Right Encoder: {right_encoder.count}"
+    )
+
+    print()
+
+    print(
+        f"Left Distance : {left_distance():.2f} in"
+    )
+
+    print(
+        f"Right Distance: {right_distance():.2f} in"
+    )
+
+    print(
+        f"Average Distance: {average_distance():.2f} in"
+    )
+
+    print()
+    print("Controls")
+    print(" W - Forward")
+    print(" S - Reverse")
+    print(" A - Left")
+    print(" D - Right")
+    print(" SPACE - Stop")
+    print(" R - Reset Encoders")
+    print(" Q - Quit")
+
+
+# ==========================================================
+# SHUTDOWN
+# ==========================================================
+
+def shutdown():
+
+    global encoder_running
+
+    encoder_running = False
+
+    stop_motor()
+
+    try:
+        keyboard_restore()
+    except:
+        pass
+
+    try:
+        pca.deinit()
+    except:
+        pass
+
+    print("\nProgram stopped.")
+
+
+# ==========================================================
+# MAIN
+# ==========================================================
+
+def main():
+
+    global encoder_running
+
+    print("Starting Goose RC Car...")
+
+    stop_motor()
+
+    encoder_thread.start()
+
+    keyboard_setup()
+
+    try:
+
+        while True:
+
+            key = key_pressed()
+
+            if key:
+
+                key = key.lower()
+
+                if key == "w":
+                    forward()
+
+                elif key == "s":
+                    backward()
+
+                elif key == "a":
+                    turn_left()
+
+                elif key == "d":
+                    turn_right()
+
+                elif key == " ":
+                    stop_motor()
+
+                elif key == "r":
+                    reset_encoders()
+
+                elif key == "q":
+                    break
+
+            print_status()
+
+            time.sleep(0.05)
+
+    except KeyboardInterrupt:
+
+        pass
+
+    finally:
+
+        shutdown()
+
+
+# ==========================================================
+# RUN PROGRAM
+# ==========================================================
+
+if __name__ == "__main__":
+
+    main()
