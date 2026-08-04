298796fd6b2f8b6ef6bf441004e17b5b802498b2
Silversan1235 <crazydj1235@gmail.com>
2026-08-04 19:47:01 -0400

    Create __init__.py

---
+++ ros2/src/goosebot/goosebot/__init__.py
@@ -0,0 +1,19 @@
+# Goosebot ROS2 Python Package
+#
+# This file marks this folder as a Python package.
+#
+# ROS2 nodes written in Python will be placed here.
+#
+# Future nodes:
+#
+# encoder_node.py
+# motor_node.py
+# robot_state_node.py
+# navigation_node.py
+#
+# Example:
+#
+# from goosebot.encoder_node import main
+#
+# ROS2 will use setup.py entry points
+# to launch these nodes.
